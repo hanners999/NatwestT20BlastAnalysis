717d70ce39ced515163378983f79487b29dc8ac6
lukewhansford <85588247+lukewhansford@users.noreply.github.com>
2021-07-31 01:25:02 +0100

    Update Cricket_data.py

--- Cricket_data.py
+++ Cricket_data.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from collections import Counter
 
 def read_file():
     directory = "/Users/Luke/Desktop/Python_work/T20_files/"
@@ -30,13 +31,11 @@ def match_stats_grabber(csv_f, info):
     "4 On-Strike Batter", "5 Off-Strike Batter", "6 Bowler", "7 Runs",
     "8 Extras", "9 Wides", "10 No-Ball", "11 Byes", "12 Leg-Byes",
     "13 Penalty", "14 How out", "15 Player Out"]"""
+    runs_breakdown = []
     runs = {}
     scorecard = {}
-    runs_breakdown = {"Name": "" , 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0}
     balls = {}
     how_out = {}
-    batting_stats = runs, balls, how_out
-    name = batting_stats
     for row in csv_f:
         if row[0] != "ball":
             continue
@@ -48,12 +47,6 @@ def match_stats_grabber(csv_f, info):
             elif batter in runs:
                 runs[batter] += int(row[7])
                 scorecard[batter] += "{}".format(row[7])
-    #        for name, scores in scorecard.items():
-    #            for score in scores:
-    #                runs_breakdown[batter] = runs_breakdown.get(score, 0)+1
-#
-#                    break
-#                break
             else:
                 print("Error in adding {}'s total".format(batter))
                 break
@@ -67,14 +60,16 @@ def match_stats_grabber(csv_f, info):
                 balls[batter] = 0
 
 ###BREAK FOR LOOP###
-    deliveries = sum(balls.values())
-    print(runs_breakdown)
-    return scorecard
-
-def score_breakdown(scorecard)
+    # This loop adds creates a breakdown of how they scored their runs and returns
+    # an array with the batters name and the breakdown
     for batter_name,scores in scorecard.items():
-        runs_breakdown["Name"] = batter_name
-        #for score in scores:
+        listed_scores = sorted(list(scores))
+        breakdown = [[x,listed_scores.count(x)] for x in set(listed_scores)]
+        runs_breakdown.append([batter_name, breakdown])
+    print(runs_breakdown)
+    return
+
+
 
 """def file_checker():
     """"""Function checks whether file to add data to exists already or not,
@@ -87,24 +82,6 @@ def score_breakdown(scorecard)
         h = open("{}/Match_Files/match{}.csv".format(directory, file_number), "x")
         print("else")
         return info_grabber()"""
-
-"""def info_grabber(rows):
-    #irrelevant_rows = [3,4,8]
-    useless_rows = ["gender", "season", "event", "city"]
-    info = {}
-    for row in rows:
-        print(info)
-        if row[0] == "version":
-            continue
-        elif row[0] == "info":
-            if row[1] in useless_rows:
-                continue
-            else:
-                #current issue is team 2 is overwriting team 1 in dict
-                info[row[1]] = row[2]
-        else:
-            break
-    return info"""
 
 
 def build_file():
